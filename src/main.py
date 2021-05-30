980497971eb467f20f3545384181407bd834e681
starr-dusT <starrtyler88@gmail.com>
2021-05-30 11:53:02 -0700

    add main and basic state cycling

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from state_machine import start_machine
+
+if __name__ == "__main__":
+    config_location = "/home/tstarr/devel/python/pyci/src/config.yml"
+    start_machine(config_location)
--- src/state_machine.py
+++ src/state_machine.py
@@ -1,37 +1,63 @@
+# imports
+import yaml
+import time
+import waiting as wt
+
 # Simple state machine where: Waiting -> Ready -> Running -> Finished -|-> Succeeded
 #                                                                      |
 #                                                                      |-> Failed
 #
 
-def waiting():
+
+def waiting(config):
     # TODO
     print("current state: waiting")
     # Next state is ready
-    return "ready"
+    return wt.eval_state(config)
 
 
-def ready():
+def ready(config):
     # TODO
-    print("current state: running")
+    print("current state: ready")
     # Next state is running
     return "running"
 
 
-def running():
+def running(config):
     # TODO
-    print("current state: finished")
+    print("current state: running")
     # Next state is finished
     return "finished"
 
 
-def finished():
+def finished(config):
     # TODO
-    print("current state: waiting")
+    print("current state: finished")
     # Next state is waiting
     return "waiting"
 
 
-states = {"waiting": waiting,
-          "ready": ready,
-          "running": running,
-          "finished": finished}
+def cycle_machine(config, states, initial_state):
+    next_state = None
+    current_state = initial_state
+
+    while 1:
+        next_state = states[current_state](config)
+        current_state = next_state
+        time.sleep(1)
+
+
+def start_machine(config_location):
+    print("starting machine...")
+    # Define the machine's states
+    states = {"waiting": waiting,
+              "ready": ready,
+              "running": running,
+              "finished": finished}
+
+    # Initalize the config file for passing
+    stream = open(config_location, "r")
+    config = yaml.safe_load(stream)
+
+    # Start the machine!
+    cycle_machine(config, states, "waiting")
