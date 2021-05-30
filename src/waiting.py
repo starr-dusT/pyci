d69bd7692369ddf8c6a1fe4f82c2005406ca474f
starr-dusT <starrtyler88@gmail.com>
2021-05-30 13:12:17 -0700

    implement naive version of waiting state

--- src/waiting.py
+++ src/waiting.py
@@ -1,17 +1,19 @@
-# import tempfile
 from config import config
+import time
 
-#def get_current_hash():
-#    # Get current hash
-#    head_loc = self.config_dict["git_repo_location"] + "/.git/logs/HEAD"
-#    head_file = open(head_loc)
-#    current_hash = head_file.read().replace("\n", " ").split(" ")[-7]
-#    if current_hash != self.last_hash:
-#        # TODO: figure out what to do here. Call a function?
-#        print("hash has changed!")
-#        self.last_hash = current_hash
-#    else:
-#        print("hash has not changed")
+def get_current_hash():
+    # Get current hash
+    head_loc = config["git_repo_location"] + "/.git/logs/HEAD"
+    head_file = open(head_loc)
+    return head_file.read().replace("\n", " ").split(" ")[-8]
+
 
 def eval_state():
-    return "ready"
+    last_hash = get_current_hash()
+    while 1:
+        current_hash = get_current_hash()
+        if current_hash != last_hash:
+            print("hash has changed!")
+            return "ready"
+        else:
+            time.sleep(1)
